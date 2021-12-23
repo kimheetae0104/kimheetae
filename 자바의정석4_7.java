cb4ecc4a4de1483d8f2c9b24f180b6157941575d
kimheetae0104 <79716614+kimheetae0104@users.noreply.github.com>
2021-12-23 22:57:41 +0900

    Add files via upload
    Math.random() function

---
+++ 자바의정석4_7.java
@@ -0,0 +1,12 @@
+package 혼자공부하기;
+
+public class 자바의정석4_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = (int)Math.random()%6+1;
+		System.out.println("value:"+value);
+		
+	}
+
+}
