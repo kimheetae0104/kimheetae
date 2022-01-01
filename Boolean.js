e37414b58fd67d0fd1f4a16b23e177d17c25f197
kimheetae0104 <79716614+kimheetae0104@users.noreply.github.com>
2022-01-02 03:59:55 +0900

    Add files via upload

---
+++ Boolean.js
@@ -0,0 +1,13 @@
+//  true, false, null, undefiend
+const amIFat = false;
+const amIFat = null;
+console.log(amIFat);
+//  null은 의도적으로 비어있다라는 것을 보여주기 위한 것
+// 값 자체가 null 즉 값이 비어있다 라는 의미 
+
+let something;
+console.log(something);
+// variavle 주는데 값이 들어가 있지 않은 것 메모리 공간은 존재 
+// 다른 타입으로 적용된다. 
+// 이건 그냥 값이 존재하지 않은 것 걍 빈 공간
+ 
