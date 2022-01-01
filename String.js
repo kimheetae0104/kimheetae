ffc2f89dcab26566476dc97e166c638a6a52bd6c
kimheetae0104 <79716614+kimheetae0104@users.noreply.github.com>
2022-01-02 03:58:50 +0900

    Add files via upload

---
+++ String.js
@@ -0,0 +1,25 @@
+// const 바뀌지 않는 수 변수를 지정해주는 함수?
+let myName = "nico";
+// 캐멜 방식으로 입력하고 파이썬에서는 중간에 언더바를 쓴다.
+// 자바스크립트는 변수지정하는 const 와 ; 는 필수
+let a = 10;
+const b = 3;
+
+//웹페이지 콘솔창에 출력하는 방법 
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(" hello "+myName);
+
+myName = "heetae";
+console.log("my name is " + myName);
+
+// variable을 만드든 방법 두가지
+// const 변수명 = 값 ; const는 상수 즉 값이 바뀔 수 없다는 뜻
+// let 변수명  = 값 ; let은 새로운 것을 생성할 때 사용하는 것 
+// 기본적으로 const 를 사용하고 필요에 따라 업데이트 해야할 코드는 let으로 쓴다.
+//  var 가 아주 오래전부터 사용하는 건데 수정을 맘대로 할 수 있음
+// var 어떤 규칙도 없는 구식 코드 never var..... ㅋㅋ
+
+
+
