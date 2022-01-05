9ffc70c91dda2230c5c69f76bba437c62cd7ca1a
kimheetae0104 <79716614+kimheetae0104@users.noreply.github.com>
2022-01-05 23:56:48 +0900

    Rename JAVA/Exam01.java to JAVA/2주차/Exam01.java

--- JAVA/2주차/Exam01.java
+++ JAVA/2주차/Exam01.java
@@ -4,7 +4,7 @@ public class Exam01 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("n �Է� : ");
+		System.out.println("n ÀÔ·Â : ");
 		int n = sc.nextInt();
 		for (int i = 0, j = 1; i < n; i++) {
 
